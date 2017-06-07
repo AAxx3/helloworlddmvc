d3ec6eb9ef302d03d71531ac59106ab7de678dfb
Ernest <Ernest@PCRP>
2017-06-07 09:20:55 +0200

    phase 2

---
+++ helloworldmaven/hellowordldmvs.model/src/main/java/org/hellowordldmvs/model/App.java
@@ -0,0 +1,13 @@
+package org.hellowordldmvs.model;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
